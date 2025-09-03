72814cdbfe4b888c30a6b6a3a7130580e61a7173
Larinaaaaa <larina.celine.klotz@audi.de>
2025-09-03 11:07:35 +0200

    #3 Create SubTasks Class

---
+++ backend/src/main/java/audi/sdc/ja_project_template/model/SubTask.java
@@ -0,0 +1,62 @@
+package audi.sdc.ja_project_template.model;
+
+public class SubTask{
+    private int id;
+    private String name;
+    private String details;
+    private boolean completed;
+    private Task task;
+
+    public SubTask(int id, String name, String details, boolean completed) {
+        this.id = id;
+        this.name = name;
+        this.details = details;
+        this.completed = completed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Subtask #%d: %s, Details: %s [completed: %s]", id, name, details, completed);
+
+    }
+}
