15b21c097bd6c7e4e070b2381dfec1c98f658f8a
Larinaaaaa <larina.celine.klotz@audi.de>
2025-09-09 09:09:00 +0200

    #10 Fixed Application Bug Created Task Repository

--- backend/src/main/java/audi/sdc/ja_project_template/Application.java
+++ backend/src/main/java/audi/sdc/ja_project_template/Application.java
@@ -3,8 +3,9 @@ package audi.sdc.ja_project_template;
 import audi.sdc.ja_project_template.repository.DatabaseConnectorSqLite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ backend/src/main/java/audi/sdc/ja_project_template/repository/TaskRepository.java
@@ -0,0 +1,13 @@
+package audi.sdc.ja_project_template.repository;
+
+import audi.sdc.ja_project_template.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+    Set<Task> findByCategory(String category);
+
+}
