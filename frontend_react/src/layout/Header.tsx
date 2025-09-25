ec1be2edfd6e07005642a8fadf0f78b5000ddcae
Larinaaaaa <larina.celine.klotz@audi.de>
2025-09-25 08:42:43 +0200

    #20 Draft of the header

--- frontend_react/src/layout/Header.tsx
+++ frontend_react/src/layout/Header.tsx
@@ -7,20 +7,19 @@ function Header() {
     return (
         <div className="header">
             <Layout justify="around" align="center">
-                <div className="logo-container">
-                    <img className="logo" src="/audi-fav.svg"></img>
-                </div>
                 <div className="text-container">
                     <p>AudiTrack</p>
                 </div>
                 <NavigationBar id="nav-bar" selected="overview" onSelect={() => {}}>
-                    <NavigationItem id="overview">Overview</NavigationItem>
+                    <NavigationItem id="overview">Übersicht</NavigationItem>
+                    <NavigationItem id="canban">Canban Board</NavigationItem>
+                    <NavigationItem id="week">Wochenansicht</NavigationItem>
+                    <div style={{ marginLeft: '50px' }}>
+                        <NavigationItem id="backlog">Backlog</NavigationItem>
+                    </div>
                 </NavigationBar>
-                <div className="button-container">
-                    <input type="file" name="" id='grades-file-import' hidden></input>
-                    <Button onClick={()=> alert("Hello!  I'm a button")} variant="secondary" icon="editorial" size="small">
-                        click me
-                    </Button>
+                <div className="logo-container">
+                    <img className="logo" src="/audi-fav.svg"></img>
                 </div>
             </Layout>
             <Divider />
