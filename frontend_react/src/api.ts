d84ceac827ee7496e91c3674ebfada3f195c14be
Larinaaaaa <larina.celine.klotz@audi.de>
2025-09-25 09:07:13 +0200

    #23 api calls for getting all tasks and subtasks

--- frontend_react/src/api.ts
+++ frontend_react/src/api.ts
@@ -1,13 +1,26 @@
-import {Animals} from "./model/Animals.ts";
+import {Task} from "./model/Task.ts";
+import {SubTask} from "./model/SubTask.ts";
 
-export async function loadAnimals(): Promise<Animals[]>{
-    const animals: Animals[] = ([]) as Animals[];
+export async function loadTasks(): Promise<Task[]>{
+    const tasks: Task[] = ([]) as Task[];
     try {
-        const response = await fetch('http://localhost:8080/api/animals');
+        const response = await fetch('http://localhost:8080/api/tasks');
         return await response.json();
 
     } catch (e) {
         console.error(e);
-        return Promise.resolve(animals);
+        return Promise.resolve(tasks);
+    }
+}
+
+export async function loadSubTasks(): Promise<SubTask[]>{
+    const subTasks: SubTask[] = ([]) as SubTask[];
+    try {
+        const response = await fetch('http://localhost:8080/api/subtasks');
+        return await response.json();
+
+    } catch (e) {
+        console.error(e);
+        return Promise.resolve(subTasks);
     }
 }
